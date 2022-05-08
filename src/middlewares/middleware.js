01c45b97d323d656815923076df9a927744c3ca1
Mateus Moreira Martins <moreiramateus758@gmail.com>
2022-05-08 12:41:23 -0300

    Subindo repositorio

---
+++ src/middlewares/middleware.js
@@ -0,0 +1,25 @@
+exports.middlewareGlobal = (req, res, next) => {
+  res.locals.errors = req.flash('errors')
+  res.locals.success = req.flash('success')
+  res.locals.user = req.session.user;
+  next();
+};
+
+exports.outroMiddleware = (req, res, next) => {
+  next();
+};
+
+exports.checkCsrfError = (err, req, res, next) =>{
+  if(err){
+    return res.render('404')
+  }
+
+  next()
+}
+
+exports.csrfMiddlewere = (req, res, next) =>{
+
+  res.locals.csrfToken = req.csrfToken()
+
+  next()
+}
